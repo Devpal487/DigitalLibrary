29389f16b8c1390181782bc17b771e8799b2cfcb
akki8418 <142257907+akki8418@users.noreply.github.com>
2024-12-09 19:35:47 +0530

    Commit_09_Dec

--- src/pages/Flipbook.jsx
+++ src/pages/Flipbook.jsx
@@ -222,9 +222,9 @@ function Flipbook({ filePath }) {
                 boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
                 transition: 'background-color 0.3s, box-shadow 0.3s',
               }}
-              onMouseEnter={() => {
-                alert("This page is not readable")
-              }}
+              // onMouseEnter={() => {
+              //   alert("This page is not readable")
+              // }}
             >
               <FontAwesomeIcon icon={faPlay} style={{ marginRight: '8px' }} />
               Start Reading
@@ -315,7 +315,7 @@ function Flipbook({ filePath }) {
       {/* Loading Spinner */}
       {loading && (
         <div className="spinner">
-          {/* <CircularProgress /> */}
+          
         </div>
       )}
       <div style={{
